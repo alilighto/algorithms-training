d8f952c674f2e103744d56ee61d18284c010d341
mohammedouchkhi <mohammedouchkhi@gmail.com>
2024-07-12 08:46:07 +0100

    updates

--- cameltosnake.go
+++ cameltosnake.go
@@ -5,21 +5,28 @@ import (
 )
 
 func main() {
-	// fmt.Println(CamelToSnakeCase("HelloWorld"))
-	// fmt.Println(CamelToSnakeCase("helloWorld"))
-	// fmt.Println(CamelToSnakeCase("camelCase"))
-	// fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
+	fmt.Println(CamelToSnakeCase("HelloWorld"))
+	fmt.Println(CamelToSnakeCase("helloworDD"))
+	fmt.Println(CamelToSnakeCase("helloWorld"))
+	fmt.Println(CamelToSnakeCase("camelCase"))
+	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
 	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))
-	// fmt.Println(CamelToSnakeCase("hey2"))
+	fmt.Println(CamelToSnakeCase("hey2"))
 }
 
 func CamelToSnakeCase(s string) string{
-	st := ""
-	for i, v := range s {
-		if v >= 'A' && v <= 'Z' && i != 0 {
+	if s == "" {
+		return ""
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 'A' && s[i] <= 'Z' && i != 0 {
+			if i+1 <= (len(s)-1) && (s[i+1] >= 'A' && s[i+1] <= 'Z') || 
+			s[i] < 'A' && s[i] > 'Z' {
+				return s
+			}
+			s = s[:i] + "_" + s[i:]
 			i++
-			st += "_" + string(s[i])
 		}
 	}
-	return st
+	return s
 }
